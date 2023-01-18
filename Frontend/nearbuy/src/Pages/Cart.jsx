8eb754eb07505f4783d913258b8040a8f42beb19
Saurav Bhardwaj <bhardwajs124@gmail.com>
2023-01-18 22:08:03 +0530

    adding cart

---
+++ Frontend/nearbuy/src/Pages/Cart.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Box} from "@chakra-ui/react"
+
+const Cart = () => {
+  return (
+    <div>
+        <Navbar />
+        <Box>
+            <h2>Cart</h2>
+        </Box>
+    </div>
+  )
+}
+
+export default Cart
