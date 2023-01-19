c443cb2a14985aa4576c9b9e8325ce2d1d5e4997
Saurav Bhardwaj <bhardwajs124@gmail.com>
2023-01-20 01:11:51 +0530

    adding checkout

--- Frontend/nearbuy/src/Routes/AllRoutes.jsx
+++ Frontend/nearbuy/src/Routes/AllRoutes.jsx
@@ -1,6 +1,7 @@
 import { Container } from "@chakra-ui/react";
 import { Routes, Route } from "react-router-dom";
 import ActivitiesDeal from "../Pages/ActivitiesDeal";
+import Checkout from "../Pages/checkout";
 import GiftCards from "../Pages/GiftCards";
 import Health from "../Pages/Health";
 import Home from "../Pages/Home";
@@ -13,6 +14,7 @@ function Router() {
         <Route path="/health" element={<Health/>} />
         <Route path="/giftcards" element={<GiftCards/>} />
         <Route path="/activitiesdeal" element={<ActivitiesDeal/>} />
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </>
   );
