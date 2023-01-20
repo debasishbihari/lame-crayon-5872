3ef6fc7695638bff3eec11b2db30bffd155afe07
shkrkumar <shekhar.rrj@gmail.com>
2023-01-20 05:46:31 +0530

    footer section completed

--- Frontend/nearbuy/src/App.jsx
+++ Frontend/nearbuy/src/App.jsx
@@ -2,6 +2,7 @@ import { Container } from '@chakra-ui/react'
 import { useState } from 'react'
 
 import './App.css'
+import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import Router from './Routes/AllRoutes'
 
@@ -12,6 +13,7 @@ function App() {
     <Container maxW='100%' minH='100vh' margin='0px' marginTop="0" textAlign='center' padding='0px'  border="5px solid red">
      <Navbar/>
      <Router/>
+     <Footer/>
     </Container>
   )
 }
